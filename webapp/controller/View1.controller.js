35faa1b0647d50f29db7552f1f5982387e2e5cba
yuhang25 <yuhang.chan@e-oasia.com>
2022-03-28 03:40:01 +0000

    commit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("Git.TestGit1.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
